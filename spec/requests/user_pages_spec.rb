17c9777a0b6e63a5178d5b54357d83e1433e503e
seinosuke <seinosuke.3606@gmail.com>
2014-09-27 11:49:32 +0900

    Chapter 6 practices

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -25,10 +25,18 @@ describe "User pages" do
     let(:submit) { "Create my account" }
 
     describe "with invalid information" do
+      # Userが新しくセーブされてない（増えていない）ことをテスト
       it "should not create a user" do
         # changeメソッド
         # シンボルを呼び出した結果を計算してオブジェクト上のメソッドとします。
         expect { click_button submit }.not_to change(User, :count)
+      end
+
+      # 誤ったUser登録でエラーが出ることをテスト
+      describe "after submission" do
+        before { click_button submit }
+        it { should have_title(full_title('Sign up')) }
+        it { should have_content('error') }
       end
     end
 
@@ -42,6 +50,16 @@ describe "User pages" do
 
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
+      end
+
+      # ユーザーを保存した後の動作を正しく捉えていることを確認
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by(email: 'user@example.com') }
+
+        it { should have_title(full_title(user.name)) }
+        # 特定のCSSクラスに属する特定のHTMLタグが存在しているかどうかをテスト
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
       end
     end
   end
